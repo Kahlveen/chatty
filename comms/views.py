6884615ee9c586630838812a58b821c1ae125f26
kahlveen <kahlveen321@gmail.com>
2018-04-25 10:34:19 +0800

    Centered input box. learnt to adjust css
    Learnt difference between # and . in css
    Classes vs id

--- comms/views.py
+++ comms/views.py
@@ -25,5 +25,5 @@ def send_msg(request):
     return render(request, 'comms/index.html', get_chat_msg())
 
 def get_chat_msg():
-    chat_msgs = ChatMsg.objects.order_by('-send_date')[:5]
+    chat_msgs = ChatMsg.objects.order_by('-send_date')[:20]
     return {'chat_msgs': reversed(chat_msgs)}
